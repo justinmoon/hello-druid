434f971a124872cda007a3615b3516a17597fb33
Justin Moon <mail@justinmoon.com>
2020-07-23 13:23:47 -0500

    Start MacOS build cleanup

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -7,29 +7,29 @@ MACOS_BIN_NAME=hello-bin
 MACOS_APP_NAME=Hello
 MACOS_APP_DIR=$MACOS_APP_NAME.app
 
-mkdir -p macbuild
-cd macbuild
-echo "Creating app directory structure"
-rm -rf $MACOS_APP_NAME
-rm -rf $MACOS_APP_DIR
+echo "Creating build directory"
+rm -rf build/macos
+mkdir -p build/macos
+cd build/macos
 mkdir -p $MACOS_APP_DIR/Contents/MacOS
 
+echo "Building Rust executable"
 cargo build --release
 
 echo "Copying binary"
 MACOS_APP_BIN=$MACOS_APP_DIR/Contents/MacOS/$MACOS_BIN_NAME
-cp ../target/release/$APP_NAME $MACOS_APP_BIN
+cp ../../target/release/$APP_NAME $MACOS_APP_BIN
 
 echo "Copying resources directory"
-cp -r ../resources $MACOS_APP_DIR/Contents/MacOS
+cp -r ../../resources $MACOS_APP_DIR/Contents/MacOS
 
 echo "Copying launcher"
-cp ../scripts/macos_launch.sh $MACOS_APP_DIR/Contents/MacOS/$MACOS_APP_NAME
+cp ../../scripts/macos_launch.sh $MACOS_APP_DIR/Contents/MacOS/$MACOS_APP_NAME
 
 echo "Copying Icon"
 mkdir -p $MACOS_APP_DIR/Contents/Resources
-cp ../resources/Info.plist $MACOS_APP_DIR/Contents/
-# cp ../resources/logo.icns $MACOS_APP_DIR/Contents/Resources/
+mv $MACOS_APP_DIR/Contents/MacOS/resources/macos/Info.plist $MACOS_APP_DIR/Contents/
+# mv $MACOS_APP_DIR/Contents/MacOS/resources/macos/logo.icns $MACOS_APP_DIR/Contents/Resources/
 
 echo "Creating dmg"
 mkdir -p $MACOS_APP_NAME
@@ -39,4 +39,9 @@ rm -rf $MACOS_APP_NAME/.Trashes
 FULL_NAME=$MACOS_APP_NAME
 
 hdiutil create $FULL_NAME.dmg -srcfolder $MACOS_APP_NAME -ov
+
+# Remove build artifacts
 rm -rf $MACOS_APP_NAME
+# rm -rf $MACOS_APP_DIR
+
+echo "Done"
