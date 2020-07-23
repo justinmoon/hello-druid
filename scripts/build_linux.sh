82992362d7379bccae882958bbb6ac0b0be74d59
Justin Moon <mail@justinmoon.com>
2020-07-23 13:10:49 -0500

    Clean up Linux build script

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-# create build/linux
-# cd
-# create appdir
-# copy the stuff (maybe make some path variables to make easier)
-
-# delete appdir
-
-rm *.AppImage
-rm -rf hello.AppDir
+# Build rust executable
 cargo build --release
-mkdir -p hello.AppDir
 
-cp -r resources hello.AppDir
+# Create clean build directory & AppImage's "AppDir" inside it
+rm -rf build/linux
+mkdir -p build/linux
+mkdir -p build/linux/hello.AppDir
 
-cp target/release/hello hello.AppDir/AppRun
+# Copy executable and resources to "AppDir"
+cp target/release/hello build/linux/hello.AppDir/AppRun
+cp -r resources build/linux/hello.AppDir
 
-cd hello.AppDir
+# Rearrange some files
+cd build/linux/hello.AppDir
 mv resources/logo.png hello.png
+mv resources/linux/hello.desktop .
 
-echo '[Desktop Entry]' > hello.desktop
-echo 'Name=hello' >> hello.desktop
-echo 'Exec=hello' >> hello.desktop
-echo 'Icon=hello' >> hello.desktop
-echo 'Type=Application' >> hello.desktop
-echo 'Categories=Finance;' >> hello.desktop
-
+# Create the final .AppImage
 cd ..
 appimagetool-x86_64.AppImage hello.AppDir
 
+# Delete build artifacts
+rm -rf hello.AppDir
